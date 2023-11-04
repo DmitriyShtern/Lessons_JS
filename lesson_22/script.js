1f2b1536d6618fb6a00d20dccb952e3d90087d4e
Дмитрий Штерн <dmitrijstern@MacBook-Air-Dmitrij.local>
2023-11-04 15:03:19 +0300

    lesson 22

---
+++ lesson_22/script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+
+
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+
+
+
+let num = 50;
+
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
+}
+
+
+
+
+
+
+
+
+
+
+
