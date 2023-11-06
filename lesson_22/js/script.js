8532219f4b94afa21d971c2399d174e465b1e4e6
Дмитрий Штерн <dmitrijstern@MacBook-Air-Dmitrij.local>
2023-11-06 20:17:19 +0300

    left the loop for

--- lesson_22/js/script.js
+++ lesson_22/js/script.js
@@ -24,16 +24,16 @@ let personalMovieDB = {
 // }
 
 
-function promis(x) {
-    for (let i = 0; i < 1; i++) {
-        x = prompt('Один из последних просмотренных фильмов?', '');
+// function promis(x) {
+//     for (let i = 0; i < 1; i++) {
+//         x = prompt('Один из последних просмотренных фильмов?', '');
     
-        if (x == '' || x == null || x.length > 50)  {
-            i--
-        };  
-    };
-    return x;
-}
+//         if (x == '' || x == null || x.length > 50)  {
+//             i--
+//         };  
+//     };
+//     return x;
+// }
 
 // const b = prompt('На сколько оцените его?', '');
 
@@ -43,23 +43,23 @@ for (let i = 0; i < 2; i++) {
     let a = '',
         b = '';
 
-        a = promis(a);
-        b = promis(b)
-    // for (let i = 0; i < 1; i++) {
-    //     a = prompt('Один из последних просмотренных фильмов?', '');
+        // a = promis(a);
+        // b = promis(b)
+    for (let i = 0; i < 1; i++) {
+        a = prompt('Один из последних просмотренных фильмов?', '');
     
-    //     if (a == '' || a == null || a.length > 50)  {
-    //         i--
-    //     };  
-    // };
+        if (a == '' || a == null || a.length > 50)  {
+            i--
+        };  
+    };
     
-    // for (let i = 0; i < 1; i++) {
-    //     b = prompt('На сколько оцените его?', '');
+    for (let i = 0; i < 1; i++) {
+        b = prompt('На сколько оцените его?', '');
         
-    //     if (b == '' || b == null || b.length > 50)  {
-    //         i--            
-    //     };
-    // };
+        if (b == '' || b == null || b.length > 50)  {
+            i--            
+        };
+    };
     personalMovieDB.movies[a] = b;
 };
 
