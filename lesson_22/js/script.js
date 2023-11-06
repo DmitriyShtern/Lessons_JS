d1cb253e3c3d87891704ac1c9347166732d48e30
Дмитрий Штерн <dmitrijstern@MacBook-Air-Dmitrij.local>
2023-11-06 20:07:51 +0300

    function

--- lesson_22/js/script.js
+++ lesson_22/js/script.js
@@ -12,7 +12,7 @@ let personalMovieDB = {
     
 };
 
-
+// ()
 // if (personalMovieDB.count < 10) {
 //     alert('Просмотренно довольно мало фильмов')
 // } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30 ) {
@@ -24,21 +24,43 @@ let personalMovieDB = {
 // }
 
 
+function promis(x) {
+    for (let i = 0; i < 1; i++) {
+        x = prompt('Один из последних просмотренных фильмов?', '');
+    
+        if (x == '' || x == null || x.length > 50)  {
+            i--
+        };  
+    };
+    return x;
+}
+
+// const b = prompt('На сколько оцените его?', '');
+
 
 for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', '');
 
-    if (a == '' || a == null || a.length > 50)  {
-        i--
-    }
-    const b = prompt('На сколько оцените его?', '');
+    let a = '',
+        b = '';
+
+        a = promis(a);
+        b = promis(b)
+    // for (let i = 0; i < 1; i++) {
+    //     a = prompt('Один из последних просмотренных фильмов?', '');
     
-    if (b == '' || b == null || b.length > 50)  {
-        b = prompt('На сколько оцените его?', '');
-    }
+    //     if (a == '' || a == null || a.length > 50)  {
+    //         i--
+    //     };  
+    // };
     
+    // for (let i = 0; i < 1; i++) {
+    //     b = prompt('На сколько оцените его?', '');
+        
+    //     if (b == '' || b == null || b.length > 50)  {
+    //         i--            
+    //     };
+    // };
     personalMovieDB.movies[a] = b;
-    
 };
 
 
