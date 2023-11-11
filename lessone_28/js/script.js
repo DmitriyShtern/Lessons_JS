da26366373e927d0f5f19c67a276598cdea8861e
Дмитрий Штерн <dmitrijstern@MacBook-Air-Dmitrij.local>
2023-11-11 13:42:51 +0300

    lessone practic  29

---
+++ lessone_28/js/script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+// let str = 'test';
+
+// str = str.toLowerCase();
+
+// console.log(str);
+
+
+
+
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("q"));
+
+
+// const logg = "Hello world";
+
+// console.log(logg.slice(6, 11));
+// console.log(parseInt(test));
+
+
+
+const test = "12.3 jkbvbkv 15 16";
+
+console.log(parseFloat(test));
+
+
+
+
+
+
+
